53d3d3a3dd0e14f65f1fccce3192922466721152
p3cet <cprzem@gmail.com>
2016-01-09 20:36:50 +0100

    static import

---
+++ static_import/src/pecetspace/Print.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pecetspace;
+
+/**
+ *
+ * @author Zwyr
+ */
+public class Print {
+    public static void println(Object obj) {
+        System.out.println(obj);
+    }   
+}
---
+++ static_import/src/staticimport/StaticImport.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staticimport;
+
+import static pecetspace.Print.*;
+
+/**
+ *
+ * @author Zwyr
+ */
+public class StaticImport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        println("Ala");
+        println(100);
+        println(10.3);
+    }
+    
+}
